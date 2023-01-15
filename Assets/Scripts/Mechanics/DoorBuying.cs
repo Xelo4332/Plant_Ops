9a4168deb0dc08c9197cfa6d3da3667e7b170e8c
Xelo4332 <mansor-deni.mamergov@elev.ga.lbs.se>
2023-01-15 01:37:45 +0100

    DoorBuyingSystemDone

---
+++ Assets/Scripts/Mechanics/DoorBuying.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class DoorBuying : MonoBehaviour
+{
+    [SerializeField] private Player _scoreDoor;
+    [SerializeField] private int _scoreNeededToBuyDoor;
+    [SerializeField] private int _scoreRemovalAfterBuy;
+
+    private void Awake()
+    {
+        _scoreDoor = FindObjectOfType<Player>();
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (_scoreDoor._score > _scoreNeededToBuyDoor)
+        {
+            if (col.gameObject.transform.name == "Player" && Input.GetKeyDown(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+                _scoreDoor._score -= _scoreNeededToBuyDoor;
+                Debug.Log(_scoreDoor);
+
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+}
