a29b02746ff5a1d761d51f4cf531f032738b3a51
Coding-boot <kacper.kijewski@elev.ga.lbs.se>
2023-01-16 14:31:27 +0100

    Revert "CrossBow"
    This reverts commit 5e1e893bd96436152fe4fd30c9ca01e5b1c18cbd, reversing
    changes made to 3610118d750efb467df5ee1ccd008b76d9a78938.

--- Assets/Scripts/Player/Weapons/CrossBow.cs
+++ Assets/Scripts/Player/Weapons/CrossBow.cs
@@ -4,49 +4,16 @@ using UnityEngine;
 
 public class CrossBow : MonoBehaviour
 {
-    [SerializeField]private Transform barrelTip;
 
-    [SerializeField] private GameObject bolt;
-
-    
-
-    [SerializeField] private float _boltSpeed;
-    [SerializeField] public int _boltDamage = 3;
-
-    private Player _player;
-    private Enemy _enemy;
     // Start is called before the first frame update
     void Start()
     {
-        _player = GetComponent<Player>();
-        _enemy = GetComponent<Enemy>();
-    }
-
-    private void Update()
-    {
-        CrossBowAttack();
-    }
-    private void CrossBowAction()
-    {
-        GameObject boltInstance = Instantiate(bolt, barrelTip.position, barrelTip.rotation);
-        boltInstance.GetComponent<Rigidbody2D>().velocity = barrelTip.up * _boltSpeed;
-        Destroy(boltInstance, 5);
-    }
-
-    private void CrossBowAttack()
-    {
-   
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-
-            CrossBowAction();
-           
-        }
-        
-
         
     }
-   
+
     // Update is called once per frame
-
+    void Update()
+    {
+        
+    }
 }
