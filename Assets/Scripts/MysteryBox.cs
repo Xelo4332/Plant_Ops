3e2f56155109208c02c42dc7ba5c5082ad2ade78
Xelo4332 <mansor-deni.mamergov@elev.ga.lbs.se>
2023-01-17 13:28:50 +0100

    MysteryBox

---
+++ Assets/Scripts/MysteryBox.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteryBox : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _weaponsDrop;
+    private Player _player;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>();
+    }
+
+    private GameObject GetRandomWeapon()
+    {
+        var index = Random.Range(0, _weaponsDrop.Length);
+        return _weaponsDrop[index];
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.transform.name == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            GetRandomWeapon();
+        }
+    }
+}
