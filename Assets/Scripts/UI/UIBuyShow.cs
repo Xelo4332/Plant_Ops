bccd26b83ee1cdf6d0154d7ebc93c4df44bbe5e7
Xelo4332 <mansor-deni.mamergov@elev.ga.lbs.se>
2023-02-08 14:25:28 +0100

    Heil

--- Assets/Scripts/UI/UIBuyShow.cs
+++ Assets/Scripts/UI/UIBuyShow.cs
@@ -6,14 +6,15 @@ using UnityEngine.UI;
 public class UIBuyShow : InteractibleItem
 {
     [SerializeField] private GameObject _UICanvas;
-    [SerializeField]
- 
+    [SerializeField] private GameObject _outLineDoor;
+   
     private new void OnTriggerEnter2D(Collider2D col)
     {
 
         if (col.TryGetComponent(out Player player))
         {
             _UICanvas.SetActive(true);
+            _outLineDoor.SetActive(true);
 
         }
     }
@@ -24,6 +25,7 @@ public class UIBuyShow : InteractibleItem
         if (col.TryGetComponent(out Player player))
         {
             _UICanvas.SetActive(false);
+            _outLineDoor.SetActive(false);
             Debug.Log("test2");
         }
     }
